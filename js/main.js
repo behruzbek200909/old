db3f031166517496c81e01caaa39e7b4afd567fd
behruzbek200909 <behruzbekmahkamov94@gmail.com>
2024-05-30 17:04:28 +0600

    old

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/////////////////////// solishtirish operatori
+
+console.log(2>6);
+console.log(2>=2);
+console.log(3<=3);
+console.log(2==2);
+
+///////////////////// tayinlash operatori
+
+let a = 4
+let b = 9
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+
